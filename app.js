43c923b78e865e3d8e7b8684e92e4ec9f6594080
Tamara Piksa <tamara.piksa@gmail.com>
2016-01-18 10:15:00 -0800

    tumblr

--- app.js
+++ app.js
@@ -32,6 +32,8 @@ writingApp.service('keywordService', function(){
 });
 
 
+
+
 writingApp.controller('homeController', ['$scope', 'keywordService', function($scope, keywordService){
 
   $scope.keyword = keywordService.keyword;
@@ -43,14 +45,29 @@ writingApp.controller('homeController', ['$scope', 'keywordService', function($s
 
 }]);
 
-writingApp.controller('twitterController', ['$scope', 'keywordService', function($scope, keywordService){
 
-  $scope.keyword = keywordService.keyword; 
+writingApp.controller('twitterController', ['$scope', '$resource', 'keywordService', function($scope, $resource, keywordService){
+
+  $scope.keyword = keywordService.keyword;
+
+  $scope.tumblrAPI =
+  $resource("https://api.tumblr.com/v2/blog/pitchersandpoets.tumblr.com/posts/photo?tag=new+york+yankees", {
+  callback: "JSON_CALLBACK"}, { get: { method: "JSONP" }});
+
+  $scope.tumblrResult = $scope.tumblrAPI.get({ q: $scope.photo, cnt: 1});
+
+  console.log($scope.tumblrResult);
+
 
 }]);
+
+
+
 
 writingApp.controller('tumblrController', ['$scope', function($scope){
 
 }]);
+
+
 
 
